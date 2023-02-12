17e9bab7ab0eecea2c339b2cd3c67926c3379f1c
Anna <lap_anna_v@mail.ru>
2023-02-12 23:20:49 +0000

    style fixed

--- src/components/organisms/Navigator/Navigator.js
+++ src/components/organisms/Navigator/Navigator.js
@@ -15,8 +15,8 @@ const Navigator = (props) => {
     <div className="navigator__wrapper ">
       {isMenuOpen && (
         <div className="navigator__list navigator__list-mobile">
-          <div className="navigator__list-mobile__inner">
-            <div className="mobile_menu mobile_menu-open" onClick={closeMenu}>
+          <div className="navigator__list-mobile__inner" onClick={closeMenu}>
+            <div className="mobile_menu mobile_menu-open" >
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M9.17 14.83L14.83 9.17M14.83 14.83L9.17 9.17M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
@@ -26,7 +26,7 @@ const Navigator = (props) => {
                 />
               </svg>
             </div>
-            <div className="navigator__list-mobile__inner-list" onCLick={closeMenu}>
+            <div className="navigator__list-mobile__inner-list" >
               {navigatorList.map((node, key) => {
                 return (
                   <Link to={`/${node.link}`} className="navigator__item" key={key}>
@@ -36,6 +36,7 @@ const Navigator = (props) => {
               })}
             </div>
           </div>
+          <div className='navigator__list-mobile__background' onClick={closeMenu}></div>
         </div>
       )}
       <div className="navigator__list navigator__list-desktop">
