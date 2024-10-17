40e21653f0aba92f212608266326930adb7ad0c0
Adam <mgm.kadiev@gmail.com>
2024-10-17 20:27:20 +0300

    add contact to footer

--- src/components/organisms/Footer/Footer.js
+++ src/components/organisms/Footer/Footer.js
@@ -11,6 +11,9 @@ class Footer extends Component {
           <div className="footer__elements">
             <div className="footer__element contact">
               <span className="vpn_icon"></span>
+              <span className="address">
+                <a href="mailto:contact@vpnlove.me">contact@vpnlove.org</a>
+              </span>
             </div>
             <div className="footer__element footer-data">VPNLOVE, 2023</div>
           </div>
